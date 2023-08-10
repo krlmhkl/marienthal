9f402fed85b7899de7fbf26ab4cf4715e1301b1a
krlmhkl <71080369+krlmhkl@users.noreply.github.com>
2023-08-10 10:15:05 +0300

    Initialize and mock data

---
+++ marienthal/src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import mock from './mocks'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ marienthal/src/mocks.js
@@ -0,0 +1,83 @@
+// mocks.js
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mock = new MockAdapter(axios);
+
+mock.onGet('/api/data').reply(200, {
+    message: 'Mocked response data',
+    // Other data as needed
+});
+
+mock.onGet(`./backend/api/job-offers/{jobOfferSecretAccessToken}`).reply(200,
+    {
+        "pageTitle": "Tööpakkumine",
+        "businessName": "Marienthali Pruulikoda OÜ",
+        "applicant": {
+            "firstName": "Mari",
+            "lastName": "Maasikas"
+        },
+
+        "welcomeText": `Ettevõte Marienthali Pruulikoda OÜ on Teile teinud tööpakkumise.
+        Pakkumise tingimustega saate tutvuda allpool. Tööpakkumise vastuvõtmiseks vajutage
+        vormi lõpus olevat nuppu 'Kinnita tööpakkumine'. Peale seda suunatakse teid uue
+        töötaja isikuandmete ankeeti täitma.`,
+
+        "contract": {
+        "contractType": "Employment",
+        "positionName": "Turundusjuht",
+        "structureTreeFullPath": "Marienthali Pruulikoda OÜ / Müük / Turundus",
+        "startDate": "2023-05-29T00:00:00",
+        "endDate": null,
+        "fteWorkload": 1.0000,
+        "remunerationType": "MonthlySalary",
+        "remunerationGrossAmount": 2750.0000,
+        "remunerationCurrency": "EUR",
+        "probationaryPeriodStartDate": "2023-05-29T00:00:00",
+        "probationaryPeriodEndDate": "2023-09-28T00:00:00"
+        },
+
+        "benefits": [
+            "7 kalendripäeva lisapuhkust aastas",
+            "Kaugtöö võimalus",
+            "Sporditoetus 33 eurot kuus",
+            "Confido terviselahenduss",
+            "Iga-aastased kogupereüritused ja koosviibimised",
+            "Isikliku mobiiltelefoni kasutamise kompensatsioon"
+        ]
+    }
+
+    )
+
+    mock.onGet(`./backend/api/new-employee-form/{jobOfferSecretAccessToken}`).reply(200,
+        {
+            "pageTitle": "Uue töötaja isikuandmete ankeet",
+            "applicant": {
+            "firstName": "Mari",
+            "lastName": "Maasikas",
+            "nationalIdentityNumber": null,
+            "dateOfBirth": null,
+            "gender": null
+            },
+            "contactDetails": {
+            "postalAddress": null,
+            "phoneNumber": null,
+            "emailAddress": "mari.maasikas@supermailer.com"
+            },
+            "bankAccount": {
+            "recipientName": "Mari Maasikas",
+            "iban": null
+            },
+            "emergencyContact": {
+            "firstName": null,
+            "lastName": null,
+            "relationshipType": null,
+            "phoneNumber": null,
+            "emailAddress": null
+            }
+        }
+    );
+
+// Add more mock endpoints as necessary
+
+export default mock;
