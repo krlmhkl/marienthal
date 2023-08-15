cfb3d08d4aa3838a03f929981bc7306ea946a09d
krlmhkl <71080369+krlmhkl@users.noreply.github.com>
2023-08-15 03:34:47 +0300

    Get necessary data from api

--- marienthal/src/views/JobOfferView.vue
+++ marienthal/src/views/JobOfferView.vue
@@ -1,4 +1,33 @@
 <script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const applicantData = ref({})
+const offerData = ref({})
+
+const getOfferData = () => {
+  axios.get(`./backend/api/job-offers/{jobOfferSecretAccessToken}`)
+    .then(response => {
+      offerData.value = response.data
+    })
+    .catch(error => {
+      console.error('Error fetching job offer data:', error);
+    })
+}
+
+const getApplicantData = () => {
+    axios.get(`./backend/api/new-employee-form/{jobOfferSecretAccessToken}`)
+    .then(response => {
+        applicantData.value = response.data
+    })
+    .catch(error => {
+        console.error('Error fetching form data:', error);
+    })
+}
+
+getOfferData()
+getApplicantData()
+
 </script>
 
 <template>
