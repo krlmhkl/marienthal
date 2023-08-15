ecec5aecb5b2d4d92e5efad41e9e3955da76bf83
krlmhkl <71080369+krlmhkl@users.noreply.github.com>
2023-08-15 03:33:08 +0300

    Get business name from api, show in header

--- marienthal/src/App.vue
+++ marienthal/src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+import axios from 'axios'
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
+
+const businessName = ref(null)
+
+const getBusinessName = () => {
+  axios.get(`./backend/api/job-offers/{jobOfferSecretAccessToken}`)
+    .then(response => {
+      businessName.value = response.data.businessName
+    })
+    .catch(error => {
+      console.error('Error fetching job offer data:', error);
+    })
+}
+
+getBusinessName()
 </script>
 
 <template>
+  <div class="bg-amber-600 flex max-w-72 flex-col align-middle">
+    <RouterLink to="/" class="font-title text-2xl md:text-4xl py-12 text-white tracking-wider self-center">{{ businessName }}</RouterLink>
+  </div>
 
   <div class="flex flex-col w-full items-center py-12">
     <RouterView />
