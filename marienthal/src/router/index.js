3ec03824e4643c1de738ff531308e0b86a72b26e
krlmhkl <71080369+krlmhkl@users.noreply.github.com>
2023-08-15 03:29:53 +0300

    Create views/components, add routes

--- marienthal/src/router/index.js
+++ marienthal/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'email',
+      component: () => import('../views/EmailView.vue')
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/job-offers/{jobOfferSecretAccessToken}',
+      name: 'job-offers',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/JobOfferView.vue')
+    },
+    {
+      path: '/new-employee-form/{jobOfferSecretAccessToken}',
+      name: 'new-employee-form',
+      component: () => import('../components/CandidateForm.vue')
     }
   ]
 })
